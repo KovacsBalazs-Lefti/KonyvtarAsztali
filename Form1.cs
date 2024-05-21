c958ee541d2d5d08296c3c1b6870a8b2314a221c
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-05-21 19:06:49 +0200

    first commit

---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonyvtarAsztali
+{
+    public partial class Form1 : Form
+    {
+        private BookService bookService;
+        public Form1()
+        {
+            InitializeComponent();
+            //előtte a grid column-nak a propertijébe meg kell adni az  oszlop nevét nagybetűvel
+            //egész sort jelölje ki- grid beállítása-"SelectionMode" fullrowselect
+            // multiselect false-ra állítása hogy ne lehessen több elemet kiválasztani
+           BooksGrid.AutoGenerateColumns = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                bookService = new BookService();
+                //frissíti a datagridet az aktuális adatokkal
+                RefreshBooksGrid();
+            }
+            catch (MySqlException ex)
+            {
+
+                MessageBox.Show(ex.Message, "Hiba történt az adatbázis kapcsolat kialakításakor");
+                this.Close();
+            }
+        }
+
+        private void RefreshBooksGrid()
+        {
+            BooksGrid.DataSource = bookService.GetBooks();
+            //törlése az auto selectnek
+            BooksGrid.ClearSelection();
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+       
+            if (BooksGrid.SelectedRows.Count == 0) 
+            {
+                MessageBox.Show("A törléshez válasszon ki könyvet!");
+                return;
+            }
+            DialogResult result = MessageBox.Show("Biztos szeretné törölni a kiválasztott könyvet?", "Biztos", MessageBoxButtons.YesNo);
+            if (result != DialogResult.Yes) 
+            {
+                return;
+            }
+
+
+            try
+            {
+                Book selected = BooksGrid.SelectedRows[0].DataBoundItem as Book;
+                if (bookService.DeleteBook(selected.Id))
+                {
+                    MessageBox.Show("Sikeres törlés");
+                }
+                else
+                {
+                    MessageBox.Show("Ez a könyv már korábban törlésre került", "Hiba történt a törlés során");
+                }
+                RefreshBooksGrid();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba történt a törlés során");
+                
+            }
+        }
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,129 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonyvtarAsztali
+{
+    internal class Statistics
+    {
+        static List<Book> books;
+        public static void Run() 
+        {
+            try
+            {
+                ReadAllBooks();
+                Console.WriteLine("500 oldalnál hosszabb könyvek száma: {0}", CountLongerThan500Pages());
+                Console.WriteLine("{0} 1950-nél réggebi könyv", OlderThan1950IsPresent() ? "Van" : "Nincs");
+                Book longest = GetLongestBook();
+                Console.WriteLine("A leghosszabb könyv:" + 
+                    "\r\n\t Szerző: {0}" +
+                    "\r\n\t Cím: {1}" +
+                    " \r\n\t Kiadás éve: {2}" +
+                    "\r\n\t Oldalszám:{3}", longest.Author, longest.Title, longest.Publish_year, longest.Page_count);
+                Console.WriteLine("A legtöbb könyvvel rendelkező szerző: {0}", GetAuthorwithMostBooks());
+                Console.Write("Adjon meg egy könyv címet: ");
+                string title = Console.ReadLine();
+                PrintAuthor(title);
+                //teszt, hogy jól csináltam a dolgokat eddig:
+                //books.ForEach(book => Console.WriteLine(book.Title));
+                Console.ReadKey();
+            }
+            catch (MySqlException ex)
+            {
+
+                Console.WriteLine("Hiba történt az adatbázis kapcsolat kiépítésekor: ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void PrintAuthor(string title)
+        {
+            int index = 0;
+            while (index < books.Count && books[index].Title != title)
+            {
+                index++;
+            }
+            if (index < books.Count)
+            {
+                Console.WriteLine("A megadott könyv szerzője: {0}", books[index].Author);
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen könyv");
+            }
+        }
+
+        private static string GetAuthorwithMostBooks()
+        {
+            Dictionary<string, int> authorBookCount = new Dictionary<string, int>();
+            foreach (Book book in books)
+            {
+                if (!authorBookCount.ContainsKey(book.Author))
+                {
+                    authorBookCount[book.Author] = 0;
+                }
+                authorBookCount[book.Author]++;
+            }
+            string author = null;
+            foreach (KeyValuePair<string, int> item in authorBookCount)
+            {
+                if (author == null)
+                {
+                    author = item.Key;
+                }
+                if (item.Value > authorBookCount[author])
+                {
+                    author = item.Key;
+                }
+            }
+            return author;
+        }
+
+        private static Book GetLongestBook()
+        {
+            Book longest = books[0];
+            for (int i = 1; i < books.Count; i++)
+            {
+                if (books[i].Page_count > longest.Page_count)
+                {
+                    longest = books[i];
+                }
+            }
+            return longest;
+        }
+
+        private static bool OlderThan1950IsPresent()
+        {
+            int index = 0;
+            while (index< books.Count && !(books[index].Publish_year < 1950))
+            {
+                index++;
+            }
+            return index < books.Count;
+        }
+
+        private static int CountLongerThan500Pages()
+        {
+            int count = 0;
+            foreach (Book book in books)
+            {
+                if (book.Page_count > 500)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private static void ReadAllBooks()
+        {
+            BookService bookService = new BookService();
+            books = bookService.GetBooks();
+        }
+    }
+}
+
